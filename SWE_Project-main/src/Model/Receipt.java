1be4c1b68cf7a2b6fec35e20c47710a7188f7f06
Bjuahdz <bryanjuarez91286@gmail.com>
2023-10-30 17:54:33 -0400

    receipt with hello

--- SWE_Project-main/src/Model/Receipt.java
+++ SWE_Project-main/src/Model/Receipt.java
@@ -3,6 +3,8 @@ package Model;
 public class Receipt {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hi my name is ipahipfdaga");
     }
     //hello output
+
 }
